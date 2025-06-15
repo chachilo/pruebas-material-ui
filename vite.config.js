a14577a7f43f4f725013838953555e2c8362f101
Rafael Galindo <rgf1996cbta3@gmail.com>
2025-06-15 00:41:49 -0600

    Modificacion de archivo vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'pruebas-material-ui' // Añade esta línea
+  base: '/pruebas-material-ui/' // Corrección: añade / al inicio y final
 })

a6b09b26ef3a9f85f82460ed7d6889af461d003d
Rafael Galindo <rgf1996cbta3@gmail.com>
2025-06-15 00:47:23 -0600

    cambios en archivo main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom' // Cambiado de BrowserRouter a HashRouter
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import theme from './theme'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
